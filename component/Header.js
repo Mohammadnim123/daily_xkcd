da49bbd53c3ca472a93af19df96e09d61dbb1503
Mohammadnim123 <mm.nemrawi96@gmail.com>
2020-10-09 13:23:59 +0300

    react 3 done

---
+++ component/Header.js
@@ -0,0 +1,28 @@
+  
+import Link from 'next/link'
+// import Style from 'next/style'
+
+export default function Header(){
+    return(
+        <>
+        <nav>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            <Link href="/about">
+                <a>About</a>
+            </Link>
+
+            </nav>
+            <h1>XKCD</h1>
+
+            <style jsx>{`
+                a{
+                    margin-right: 5px;
+                    color: green;
+                }
+            `}
+            </style>
+            </>
+    )
+}
